68f3e7244774760a6c58454e03e440a67aa9804c
Roman Sharov <roman.sharov@web.de>
2026-01-30 14:28:53 +0100

    Favicon

---
+++ .next/dev/server/chunks/ssr/Desktop_MyWebInGit_portfolio-sharov_app_9cb68edb._.js
@@ -0,0 +1,22 @@
+module.exports = [
+"[project]/Desktop/MyWebInGit/portfolio-sharov/app/icon.png (static in ecmascript, tag client)", ((__turbopack_context__) => {
+
+__turbopack_context__.v("/_next/static/media/icon.b0c81370.png");}),
+"[project]/Desktop/MyWebInGit/portfolio-sharov/app/icon.png.mjs { IMAGE => \"[project]/Desktop/MyWebInGit/portfolio-sharov/app/icon.png (static in ecmascript, tag client)\" } [app-rsc] (structured image object, ecmascript)", ((__turbopack_context__) => {
+"use strict";
+
+__turbopack_context__.s([
+    "default",
+    ()=>__TURBOPACK__default__export__
+]);
+var __TURBOPACK__imported__module__$5b$project$5d2f$Desktop$2f$MyWebInGit$2f$portfolio$2d$sharov$2f$app$2f$icon$2e$png__$28$static__in__ecmascript$2c$__tag__client$29$__ = __turbopack_context__.i("[project]/Desktop/MyWebInGit/portfolio-sharov/app/icon.png (static in ecmascript, tag client)");
+;
+const __TURBOPACK__default__export__ = {
+    src: __TURBOPACK__imported__module__$5b$project$5d2f$Desktop$2f$MyWebInGit$2f$portfolio$2d$sharov$2f$app$2f$icon$2e$png__$28$static__in__ecmascript$2c$__tag__client$29$__["default"],
+    width: 1544,
+    height: 1623
+};
+}),
+];
+
+//# sourceMappingURL=Desktop_MyWebInGit_portfolio-sharov_app_9cb68edb._.js.map
